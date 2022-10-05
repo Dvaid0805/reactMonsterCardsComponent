ac2b23b7c39c5f0481f743d7911096bc58311ded
Davyd Bredykhin <correodemimobil@gmail.com>
2022-10-05 12:39:28 +0200

    removed this sign ;

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -6,7 +6,7 @@ const CardList = ({monsters}) => (
   <div className="card-list">
     {monsters.map(monster => {
       return <CardItem key={monster.id} monster={monster}/>;
-    })};
+    })}
   </div>
 );
 
